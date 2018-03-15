8a0de57446d3ab8a6051c99d0fd3e8160fd3cd0c
Tom Storm <tstorm@invoca.com>
2018-03-15 13:44:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 
 class Dog
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
 
-  def initialize(name)
-    @name = name
-  end
- 
   def name
-    @name
-  end
- 
-  def name=(new_name)
-    @name = new_name
+    @this_dogs_name
   end
 end
+
+fido = Dog.new
+fido.name = "Fido"
+
+puts fido.name
